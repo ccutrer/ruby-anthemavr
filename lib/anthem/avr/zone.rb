7f13e8d2dc930ad80d220a7f837b749a37746baa
Cody Cutrer <cody@cutrer.us>
2021-05-08 19:41:58 -0600

    custom message support
    not currently exposed via MQTT

--- lib/anthem/avr/zone.rb
+++ lib/anthem/avr/zone.rb
@@ -21,6 +21,38 @@ module Anthem
     end
 
     class Zone1 < Zone
+      def custom_message(message)
+        lines = message.split("\n")[0..3]
+
+        # auto line wrap if given a single line
+        if lines.length == 1 && lines[0].length > 32
+          original = lines[0]
+          lines = []
+          while lines.length <= 3 do
+            break if original.empty?
+
+            # find the max length that fits
+            # this method preserves interior spaces
+            i = original.length
+            loop do
+              break if i < 32 || i.nil?
+
+              i = original.rindex(/\s/, i - 1)
+            end
+            if i.nil?
+              lines << original[0...32]
+              original.clear
+            else
+              lines << original.slice!(0..i).strip
+            end
+          end
+        end
+
+        lines.each_with_index do |line, i|
+          @avr.command("Z1MSG#{i}#{line[0..32]}")
+        end
+      end
+
       def resolution
         return "#{horizontal_resolution}x#{vertical_resolution}" if horizontal_resolution.to_i != 0 && vertical_resolution.to_i != 0
 
